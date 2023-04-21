ab63da65f46deca545c29781d48892e541858c83
ふぁ <yuki@yuki0311.com>
2023-04-22 06:06:57 +0900

    fix error
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,28 @@ import React from "react";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
-function App() {
+const App = () => {
+  const HideResolverErrorsPlugin = () => {
+    return {
+      statePlugins: {
+        err: {
+          wrapSelectors: {
+            allErrors: (ori: () => any[]) => () => {
+              return ori().filter((err) => err.get("source") !== "resolver");
+            },
+          },
+        },
+      },
+    };
+  };
+
   return (
-    <SwaggerUI url="https://raw.githubusercontent.com/fa0311/twitter-openapi/main/dist/openapi-3.0.yaml" />
+    <SwaggerUI
+      url="assets/openapi/openapi-3.0.yaml"
+      // url="https://raw.githubusercontent.com/fa0311/twitter-openapi/main/dist/openapi-3.0.yaml"
+      plugins={[HideResolverErrorsPlugin]}
+    />
   );
-}
+};
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
 export default defineConfig({
-  plugins: [react()],
   base: "./",
   build: {
     outDir: "docs",
   },
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "twitter-openapi/dist/*",
+          dest: "assets/openapi",
+        },
+      ],
+    }),
+  ],
 });
